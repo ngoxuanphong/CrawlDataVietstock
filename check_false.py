7eeef9735556186cff8346272f13f6cda903746d
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-06-21 17:23:13 +0700

    hihi

--- check_false.py
+++ check_false.py
@@ -40,8 +40,8 @@ for folder in list_folder:
     if (os.path.exists(folder) == False) or (os.path.isdir(folder) == False):
         os.mkdir(folder)
 
-# web = F.FinanStatement()
-# web.login(user,password)
+web = F.FinanStatement()
+web.login(user,password)
 import multiprocessing
 # web_infor = LI.ListingInformation()
 # web_infor.login(user,password)
@@ -49,43 +49,21 @@ import multiprocessing
 # web_other = O.Other()
 # web_other.login(user,password)
 
-def run1(symbol):
-    web_other.CashDividend(symbol).to_csv(f'{p1_CashDividend}{symbol}.csv', index=False)
-    web_other.StockDividend(symbol).to_csv(f'{p1_StockDividend}{symbol}.csv', index=False)
-    web_other.AdditionalListing(symbol).to_csv(f'{p1_AdditionalListing}{symbol}.csv', index=False)
-    web_other.TreasuryStockTransactions(symbol).to_csv(f'{p1_TreasuryStockTransactions}{symbol}.csv', index=False)
-    web_other.Company_delisting(symbol).to_csv(f'{p1_Company_delisting}{symbol}.csv', index=False)
 
-def run2(symbol):
-    web_infor.lst_infor(symbol).to_csv(f'{p_VolumeNow}{symbol}.csv', index=False)
+data_false = json.load(open('false.json'))
+for symbol in data_false[ps_y_BS]:
+    web.BalanceSheet(symbol, 'NAM').to_csv(f'{ps_y_BS}{symbol}.csv', index = False)
+for symbol in data_false[ps_y_IS]:
+    web.IncomStatement(symbol, 'NAM').to_csv(f'{ps_y_IS}{symbol}.csv', index = False)
+for symbol in data_false[ps_y_CF]:
+    web.CashFlows(symbol, 'NAM').to_csv(f'{ps_y_CF}{symbol}.csv', index = False)
 
-# for i in range(489,len(all_com)):
-#     symbol = all_com[i]
-#     print(i, symbol)
-#     web.BalanceSheet(symbol, 'NAM').to_csv(f'{p1y_BalanceSheet}{symbol}.csv', index = False)
-#     web.IncomStatement(symbol, 'NAM').to_csv(f'{p1y_IncomStatement}{symbol}.csv', index = False)
-    # web.CashFlows(symbol, 'NAM').to_csv(f'{p1y_CashFlows}{symbol}.csv', index = False)
-
-    # web.BalanceSheet(symbol, 'QUY').to_csv(f'{p1q_BalanceSheet}{symbol}.csv', index = False)
-    # web.IncomStatement(symbol, 'QUY').to_csv(f'{p1q_IncomStatement}{symbol}.csv', index = False)
-    # web.CashFlows(symbol, 'QUY').to_csv(f'{p1q_CashFlows}{symbol}.csv', index = False)
-
-
-# data_false = json.load(open('false.json'))
-# for symbol in data_false[p1q_BalanceSheet]:
-#     web.BalanceSheet(symbol, 'QUY').to_csv(f'{p1q_BalanceSheet}{symbol}.csv', index = False)
-# for symbol in data_false[p1y_BalanceSheet]:
-#     web.BalanceSheet(symbol, 'NAM').to_csv(f'{p1y_BalanceSheet}{symbol}.csv', index = False)
-# for symbol in data_false[p1q_IncomStatement]:
-#     web.IncomStatement(symbol, 'QUY').to_csv(f'{p1q_IncomStatement}{symbol}.csv', index = False)
-# for symbol in data_false[p1y_IncomStatement]:
-#     web.IncomStatement(symbol, 'NAM').to_csv(f'{p1y_IncomStatement}{symbol}.csv', index = False)
-# for symbol in data_false[p1q_CashFlows]:
-#     print(symbol)
-#     web.CashFlows(symbol, 'QUY').to_csv(f'{p1q_CashFlows}{symbol}.csv', index = False)
-# for symbol in data_false[p1y_CashFlows]:
-#     print(symbol)
-#     web.CashFlows(symbol, 'NAM').to_csv(f'{p1y_CashFlows}{symbol}.csv', index = False)
+for symbol in data_false[ps_q_BS]:
+    web.BalanceSheet(symbol, 'QUY').to_csv(f'{ps_q_BS}{symbol}.csv', index = False)
+for symbol in data_false[ps_q_IS]:
+    web.IncomStatement(symbol, 'QUY').to_csv(f'{ps_q_IS}{symbol}.csv', index = False)
+for symbol in data_false[ps_q_CF]:
+    web.CashFlows(symbol, 'QUY').to_csv(f'{ps_q_CF}{symbol}.csv', index = False)
 
 # web.CashFlows('AAM', 'QUY').to_csv(f'{p1q_CashFlows}AAM.csv', index = False)
 # web.CashFlows('AAM', 'QUY').to_csv(f'{p1q_CashFlows}AAM.csv', index = False)
@@ -107,27 +85,11 @@ def run2(symbol):
 # web_other.Listing().to_csv('Listing.csv', index = False)
 # web_other.Delisting().to_csv('Delisting.csv', index = False)
 
-# def multip():
-#     Symbol = all_com
-#     pool = multiprocessing.Pool(processes=4)
-#     for symbol in Symbol:
-#       pool.apply_async(run1,args=(symbol,))
-#     pool.close()
-#     pool.join()
-# if __name__ == '__main__':
-#     multip()
 
 # list_bs_year_false = ['ACV', 'AME', 'AMV', 'APF', 'ATB', 'CLG', 'DNY']
-list_bs_year_false = ['ABS', 'AUM', 'BAM', 'CLL', 'CTI']
-# list_bs_year_false = ['AAA', 'AAT', 'AAV', 'ABC', 'ABS', 'ABT', 'ACC', 'ADC', 'ADP', 'ADS', 'AFX', 'AGC', 'AGF', 'ALC', 'ALP', 'ALT', 'ALV', 'AMS', 'AMV', 'APC', 'APH', 'API', 'ASG', 'AST', 'ATB', 'AUM', 'AVC', 'B82', 'BAM', 'BAS', 'BAX', 'BBL', 'BBM', 'BCG', 'BCI', 'BDB', 'BDC', 'BDG', 'BDP', 'BDT', 'BDW', 'BED', 'BEL', 'BFC', 'BGW', 'BHA', 'BKG', 'BLF', 'BMP', 'BMV', 'BNC', 'BNW', 'BPC', 'BRC', 'BTC', 'BTV', 'C69', 'CAD', 'CAG', 'CEC', 'CIG', 'CLL', 'CLM', 'CLX', 'CNG', 'CSM', 'CT6', 'CTI', 'CTT', 'DAE', 'DAT', 'DGL', 'DHC', 'DIH', 'DP1', 'DPC', 'DST', 'DTD', 'DTI']
-list_bs_year_false_ic = ['ABR', 'ACG', 'AGP', 'AMS', 'ATA', 'BAM', 'BBL', 'BPC', 'BPW', 'BRR', 'BSQ', 'BTB', 'CAD', 'CEC', 'CTF', 'DBC', 'DDN', 'DGC', 'DHG', 'DHL', 'DLD', 'DLG', 'DPS', 'DRI']
-list_bs_year_false_ic = ['CAD']
-# for symbol in ['AAA', 'BHN', 'CCP']:
-    # web.IncomStatement(symbol, 'NAM').to_csv(f'base/Phase1/false/{symbol}.csv', index = False)
+# list_bs_year_false = ['AC4', 'ATD', 'BBH', 'BTJ', 'BXT', 'CE1', 'CNA', 'DSS']
+# for symbol in list_bs_year_false:
+#     web.IncomStatement(symbol, 'NAM').to_csv(f'base/Phase1/false/{symbol}.csv', index = False)
     # web.CashFlows(symbol, 'QUY').to_csv(f'base/Phase1/false/{symbol}.csv', index = False)
     # web.BalanceSheet(symbol, 'NAM').to_csv(f'base/Phase1/false/{symbol}.csv', index = False)
-list_infor_false = ['24H', 'ABG', 'AGB', 'AGE', 'ALC', 'ALS', 'ATC', 'ATP', 'AVG', 'BAV', 'BBL', 'BCH', 'BCO', 'BGT', 'BJC', 'BNC', 'BPT', 'BRM', 'BRV', 'BTA', 'BTJ', 'BTL', 'BVA', 'BVC', 'C42', 'CAF', 'CAR', 'CBV', 'CC7', 'CDV', 'CGC', 'CIE', 'CIV', 'CJV', 'CK8', 'CKI', 'CKM', 'CNS', 'CNX', 'CPT', 'CRV', 'CTE', 'CXC', 'DBV', 'DHQ', 'DKW', 'DLM', 'DLX', 'DMH', 'DNG', 'DNV', 'DO3', 'DTF', 'DTJ']
-
-
-# for symbol in list_infor_false:
-#      web_infor.lst_infor(symbol).to_csv(f'base/Phase1/false/{symbol}.csv', index = False)
+# list_infor_false = ['24H', 'ABG', 'AGB', 'AGE', 'ALC', 'ALS', 'ATC', 'ATP', 'AVG', 'BAV', 'BBL', 'BCH', 'BCO', 'BGT', 'BJC', 'BNC', 'BPT', 'BRM', 'BRV', 'BTA', 'BTJ', 'BTL', 'BVA', 'BVC', 'C42', 'CAF', 'CAR', 'CBV', 'CC7', 'CDV', 'CGC', 'CIE', 'CIV', 'CJV', 'CK8', 'CKI', 'CKM', 'CNS', 'CNX', 'CPT', 'CRV', 'CTE', 'CXC', 'DBV', 'DHQ', 'DKW', 'DLM', 'DLX', 'DMH', 'DNG', 'DNV', 'DO3', 'DTF', 'DTJ']
--- test.py
+++ test.py
@@ -28,7 +28,8 @@ p_DividendShares = f'{p}Dividend/DividendShares/'
 p_TreasuryShares = f'{p}Volume/TreasuryShares/'
 p_VolumeAdditionalEvents = f'{p}Volume/VolumeAdditionalEvents/'
 p_VolumeNow = f'{p}Volume/VolumeNow.csv/'
-
+p_DataUpDownExchange = f'{p}DataUpDownExchange/'
+p_DataDownExchange = f'{p}DataUpDownExchange/DataDownExchange/'
 
 all_com = pd.read_excel('base/Phase1/List_Com_Phase2.xlsx')['Symbol']
 all_com = list(all_com)
@@ -36,28 +37,29 @@ list_folder=[p, ps_finan, ps_y, ps_q,
             ps_y_BS, ps_y_CF, ps_y_IS, 
             ps_q_BS, ps_q_CF, ps_q_IS,
             p_Dividend, p_Volume, 
-            p_DividendCash, p_DividendShares, p_TreasuryShares, p_VolumeAdditionalEvents, p_VolumeNow]
+            p_DividendCash, p_DividendShares, p_TreasuryShares, p_VolumeAdditionalEvents, p_VolumeNow,
+            p_DataUpDownExchange,p_DataDownExchange]
 for folder in list_folder:
     if (os.path.exists(folder) == False) or (os.path.isdir(folder) == False):
         os.mkdir(folder)
 
 all_com = pd.read_excel('base/Phase1/List_Com_Phase2.xlsx')['Symbol']
 all_com = list(all_com)
-web = F.FinanStatement()
-web.login(user,password)
+# web = F.FinanStatement()
+# web.login(user,password)
 # web = LI.ListingInformation()
 # web.login(user,password)
-# web = O.Other()
-# web.login(user,password)
+web = O.Other()
+web.login(user,password)
 def run2(symbol):
     web.lst_infor(symbol).to_csv(f'{p_VolumeNow}{symbol}.csv', index=False)
 
 def run1(symbol):
-    web.CashDividend(symbol).to_csv(f'{p_DividendCash}{symbol}.csv', index=False)
-    web.StockDividend(symbol).to_csv(f'{p_DividendShares}{symbol}.csv', index=False)
-    web.AdditionalListing(symbol).to_csv(f'{p_VolumeAdditionalEvents}{symbol}.csv', index=False)
-    web.TreasuryStockTransactions(symbol).to_csv(f'{p_TreasuryShares}{symbol}.csv', index=False)
-    # web.Company_delisting(symbol).to_csv(f'{p1_Company_delisting}{symbol}.csv', index=False)
+    # web.CashDividend(symbol).to_csv(f'{p_DividendCash}{symbol}.csv', index=False)
+    # web.StockDividend(symbol).to_csv(f'{p_DividendShares}{symbol}.csv', index=False)
+    # web.AdditionalListing(symbol).to_csv(f'{p_VolumeAdditionalEvents}{symbol}.csv', index=False)
+    # web.TreasuryStockTransactions(symbol).to_csv(f'{p_TreasuryShares}{symbol}.csv', index=False)
+    web.Company_delisting(symbol).to_csv(f'{p_DataDownExchange}{symbol}.csv', index=False)
 def run3(symbol):
     web.BalanceSheet(symbol, 'NAM').to_csv(f'{ps_y_BS}{symbol}.csv', index = False)
     web.IncomStatement(symbol, 'NAM').to_csv(f'{ps_y_IS}{symbol}.csv', index = False)
@@ -68,9 +70,9 @@ def run3(symbol):
     web.CashFlows(symbol, 'QUY').to_csv(f'{ps_q_CF}{symbol}.csv', index = False)
 
 
-for i in range(398, len(all_com)):
+for i in range(0, len(all_com)):
     symbol = all_com[i]
     print(i, symbol)
     # run2(symbol)
-    # run1(symbol)
-    run3(symbol)
+    run1(symbol)
+    # run3(symbol)
